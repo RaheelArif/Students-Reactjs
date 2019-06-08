3bc4ce41dd81fc72b3899594d6b1530e95cc084e
RaheelArif <phuawei292@gmail.com>
2019-06-08 16:19:00 +0500

    second lecture state and class base component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
+import Header from "./components/Header";
+import "./App.css"
+class App extends Component {
+  state = {
+    name : "ahmed",
+    age: 24
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+         <h1>your name is {this.state.name}</h1>
+         <p>your age is {this.state.age}</p>
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+          
+         <h1> heading from new component</h1>
+         
+         <p>paragraph from header</p>
+      </div>
+
+    )
+  }
+}
+
+export default Header;
