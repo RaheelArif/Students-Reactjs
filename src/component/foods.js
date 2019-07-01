9e34f28b91a272c9dc8dac35ec7940d9c99456bd
RaheelArif <phuawei292@gmail.com>
2019-07-01 19:11:12 +0500

    spread operator set state

--- src/component/foods.js
+++ src/component/foods.js
@@ -3,42 +3,51 @@ import "../App.css";
 
 class Foods extends Component {
     state = {
-        userName: "ali",
-        firstName: "asif",
-        lastName: "arif"
+        firstName: undefined,
+        lastName: undefined,
+        student: [
+            { firstName: "ali", lastName: "asif" }
+
+        ]
     }
+handleChange =(e) => {
+    this.setState({
+     [e.target.name]: e.target.value ,
+    })
+}
 
-
-    handleChange = (e) => {
-     this.setState({
-
-         [e.target.name] : e.target.value 
-     })
+addItem = (e) => {
+    let newStudent = {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName
     }
+    this.setState({
+      student: [...this.state.student , newStudent]
+       })
+
+}
 
 
-    
-  
+
+
     render() {
         return (
             <div>
                 <h1 >our menu</h1>
-                <input name="userName" onChange={this.handleChange} type="text" />
-                <input name="lastName" onChange={this.handleChange} type="text" />
                 <input name="firstName" onChange={this.handleChange} type="text" />
+                <input name="lastName" onChange={this.handleChange} type="text" />
                 <button onClick={this.addItem}>new name</button>
-                <h1>user name: {this.state.userName}</h1>
-                <h1>last name: {this.state.lastName}</h1>
-                <h1>first name: {this.state.firstName}</h1>
-                {/* {
-                    this.state.map(item => {
+     <h1>{this.state.firstName}</h1>
+                {
+                    this.state.student.map(item => {
                         return (
-                            <div className="menu-area" key={item.id}>
-                                <h1 className="menu">{item.foodName}</h1>
+                            <div className="menu-area">
+                                <h1 className="menu">{item.firstName} </h1>
+                                <h1 className="menu"> : {item.lastName}</h1>
                             </div>
                         )
                     })
-                } */}
+                }
 
             </div>
         )
